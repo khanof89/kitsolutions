8eeba2f3e1597d4149c38367e8c55aa3ef33f177
kmanjari <kanakmanjari@gmail.com>
2016-09-23 12:52:41 +0530

    first_push

---
+++ database/migrations/2016_09_23_065051_create_slider_texts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSliderTextsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slider_texts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('text');
+            $table->integer('slider_id')->unsigned();
+            $table->foreign('slider_id')->references('id')->on('sliders')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slider_texts');
+    }
+}
---
+++ database/migrations/2016_09_23_065519_create_sub_menus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('url');
+            $table->integer('menu_id');
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_menus');
+    }
+}
